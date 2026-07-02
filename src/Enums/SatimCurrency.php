59cc179f7e141d3daf0bf530dfd83eb29399fdce
Abderrahim CHETIBI <chetibi.abderrahim@gmail.com>
2026-07-02 14:33:22 +0100

    Remove EnumToArray trait and associated usages from Enums

--- src/Enums/SatimCurrency.php
+++ src/Enums/SatimCurrency.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace LaravelSatim\Enums;
 
-use LaravelSatim\Traits\EnumToArray;
-
 enum SatimCurrency: string
 {
-    use EnumToArray;
-
     case DZD = '012';
     // case EUR = '978';
     // case USD = '840';
--- src/Enums/SatimLanguage.php
+++ src/Enums/SatimLanguage.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace LaravelSatim\Enums;
 
-use LaravelSatim\Traits\EnumToArray;
-
 enum SatimLanguage: string
 {
-    use EnumToArray;
-
     case AR = 'AR';
     case EN = 'EN';
     case FR = 'FR';
